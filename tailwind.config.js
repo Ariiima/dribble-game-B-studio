def9559764e5bf80d951ca151536f1cf8e096ee7
Kazuha <nasrabadihamidreza@gmail.com>
2022-02-16 22:41:01 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'button-p': "url('/dist/buttonB.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",      }
+    },
+  },
+  plugins: [],
+}
